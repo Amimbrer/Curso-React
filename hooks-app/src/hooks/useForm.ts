21282d68416018b22215212416489f27dccc0333
Amimbrer <136268910+Amimbrer@users.noreply.github.com>
2024-05-20 22:12:42 +0200

    Seccion 14
    react-heroes

--- hooks-app/src/hooks/useForm.ts
+++ hooks-app/src/hooks/useForm.ts
@@ -1,18 +1,17 @@
 import { useState } from "react";
 import { FormHook } from "../interfaces/IForm"
 
-// TIPOS E INTERFACES
-type InputChangeProps = {
-    target: HTMLInputElement
-}
 
 export const useForm = (initialForm: FormHook = {}) => {
+    // TIPOS E INTERFACES
+    type InputChangeProps = {
+        target: HTMLInputElement
+    }
 
     const [formState, setFormSate] = useState(initialForm);
 
-    const onInputChange = ({ target }: InputChangeProps
-    ) => {
-
+    // Evento change que accede al tarjet de la propiedad event
+    const onInputChange = ({ target }: InputChangeProps) => {
         const { name, value } = target;
 
         setFormSate({
